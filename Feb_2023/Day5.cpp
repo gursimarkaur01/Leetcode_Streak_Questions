4db192085447d5edabd65dda0a4ca63e0a94cc0b
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-02-06 23:20:48 +0530

    Updated

--- Feb_2023/Day5.cpp
+++ Feb_2023/Day5.cpp
@@ -1,2 +1,23 @@
 //Question: Find All Anagrams in a String
 //Source: "https://leetcode.com/problems/find-all-anagrams-in-a-string"
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int>hash1(26,0),hash2(26,0);
+        int n=p.size(),m=s.size();
+        for(int i=0;i<n;i++){
+            hash1[p[i]-'a']++;
+        }
+       
+        int i=0,j=0,c=0;
+        vector<int>ans;
+        while(i<m){
+            hash2[s[i]-'a']++;
+            if(i>=n) hash2[s[j++]-'a']--;
+            if(hash1==hash2)ans.push_back(i-n+1);
+            i++;
+        }
+        return ans;
+    }
+};
