14d3a59debc0e5a26dc5680e8591deed6aa97ca9
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-03-01 21:50:14 +0530

    Added Day1

---
+++ March_2023/Day1.cpp
@@ -0,0 +1,12 @@
+/*  Question: Sort an Array
+    Source: leetcode.com/problems/sort-an-array/
+    
+    CODE:                                                                                                  */
+class Solution {
+    public:
+    vector<int> sortArray
+    (vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
