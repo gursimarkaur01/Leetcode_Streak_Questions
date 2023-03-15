5546782aae18f681a451ed00f3f47f4ee66fbb4d
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-03-15 19:47:12 +0530

    Added Day15

---
+++ March_2023/Day15.cpp
@@ -0,0 +1,29 @@
+/*  Question: 
+    Source: https://leetcode.com/problems/check-completeness-of-a-binary-tree/
+
+    CODE:                                                                                 */
+
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        if(!root) return true; 
+        queue<TreeNode*>q;
+        q.push(root);
+        bool nullSeen=false; 
+
+        while(!q.empty()){ 
+            int levelSize=q.size(); 
+            while(levelSize--){    
+                TreeNode* tp=q.front();
+                q.pop();
+                if(!tp) nullSeen=true;  
+                else{
+                    if(nullSeen) return false; 
+                    q.push(tp->left);
+                    q.push(tp->right);
+                }
+            }
+        }
+        return true;
+    }
+};
