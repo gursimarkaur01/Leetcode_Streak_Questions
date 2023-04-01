7aad28afa3ee886af64cc2c1b605dbbe93c1e413
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-04-01 22:51:09 +0530

    Added Day1

---
+++ April_2023/Day1.cpp
@@ -0,0 +1,23 @@
+/*  Question: Binary Search
+    Source: https://leetcode.com/problems/binary-search/
+    
+    CODE:                                                    */
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int beg = 0;
+        int end = nums.size() - 1;
+        while (beg <= end) {
+            int mid = beg + (end - beg) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                beg = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
