b81bbee755219b0adbef6447f7309705f5463278
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-04-02 20:48:07 +0530

    Added Day2

--- April_2023/Day2.cpp
+++ April_2023/Day2.cpp
@@ -6,16 +6,13 @@
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        vector<int> numPairs(spells.size(), 0);
-        for(int i = 0; i < spells.size(); i++) { 
-            int spell = spells[i];
-            int count = 0;
-            for(int potion: potions) { 
-                long long pair = (long long)potion * (long long)spell;
-                if(pair >= success) count++;
-            }
-            numPairs[i] = count;
+        sort(potions.begin(), potions.end());
+        vector<int> res;
+        for (int spell: spells) {
+            long potion_needed = (success + spell - 1) / spell;
+            auto it = lower_bound(potions.begin(), potions.end(), potion_needed);
+            res.push_back(potions.end() - it);
         }
-        return numPairs;
+        return res;
     }
 };
