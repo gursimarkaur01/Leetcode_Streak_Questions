71fddf525112c306c2795b645458e44f028e6623
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-04-04 20:26:14 +0530

    Added Day4

---
+++ April_2023/Day4.cpp
@@ -0,0 +1,21 @@
+/*  Question: Optimal Partition of String
+    Source: https://leetcode.com/problems/optimal-partition-of-string/
+
+    CODE:                                                                      */
+
+class Solution {
+public:
+    int partitionString(string s) {
+        int n=s.size();
+        unordered_map<char,int>m;
+        int ans=1;
+        for(int i=0;i<n;i++){
+            if(m.count(s[i])){
+              ans++;
+                m.clear();
+            }
+            m[s[i]]=i;
+        }
+        return ans;
+    }
+};
