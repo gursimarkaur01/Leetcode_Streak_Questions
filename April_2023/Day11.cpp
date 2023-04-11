01894a30b9e5debb52dd11ce4ee73bcacc51e689
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-04-11 20:55:52 +0530

    Added Day11.cpp

---
+++ April_2023/Day11.cpp
@@ -0,0 +1,25 @@
+/*  Question: Removing Stars From a String 
+    Source: https://leetcode.com/problems/removing-stars-from-a-string/
+
+    CODE:                                             */
+
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '*' && !st.empty()) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        string ans;
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
