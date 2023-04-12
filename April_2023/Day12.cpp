b01ec73bce3dc976aa1e76bb2f18eddd729281f4
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-04-12 20:01:15 +0530

    Added Day12

---
+++ April_2023/Day12.cpp
@@ -0,0 +1,43 @@
+/*  Question: Simplify Path
+    Source: https://leetcode.com/problems/simplify-path/
+    
+    CODE:                                                    */
+
+class Solution {
+public:
+    string simplifyPath(string path) {
+        
+        stack<string> st;
+        string res;
+        
+        for(int i = 0;  i<path.size(); ++i)
+        {
+            if(path[i] == '/')    
+                continue;
+            string temp;
+            while(i < path.size() && path[i] != '/')
+            {
+				temp += path[i];
+                ++i;
+            }
+            if(temp == ".")
+                continue;
+                if(temp == "..")
+            {
+                if(!st.empty())
+                    st.pop();
+            }
+            else
+                st.push(temp);
+        }
+        while(!st.empty())
+        {
+            res = "/" + st.top() + res;
+            st.pop();
+        }
+        if(res.size() == 0)
+            return "/";
+        
+        return res;
+    }
+};
