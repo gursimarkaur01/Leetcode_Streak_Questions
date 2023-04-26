b297a89cfec49afc82db352a83152d550b968876
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-04-26 20:54:17 +0530

    Added Day26

---
+++ April_2023/Day26.cpp
@@ -0,0 +1,18 @@
+/*  Question: Add Digits
+    Source: https://leetcode.com/problems/add-digits/
+
+    CODE:                                               */
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        if (num % 9 == 0) {
+            return 9;
+        } else {
+            return (num % 9);
+        }
+    }
+};
