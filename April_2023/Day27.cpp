7b37c8f40bb7491a15f936b27f978ba17d815844
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-04-27 22:15:18 +0530

    Added Day27

---
+++ April_2023/Day27.cpp
@@ -0,0 +1,11 @@
+/*  Question: Bulb Switcher
+    Source: https://leetcode.com/problems/bulb-switcher/
+
+    CODE:                                                */
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
