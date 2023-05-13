d6105a6df197c8a9c3d2d05d4101e46db2968663
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-05-13 18:46:13 +0530

    Added Day13

---
+++ May_2023/Day13.cpp
@@ -0,0 +1,26 @@
+/*  Question:  Count Ways to Build Good Strings
+    Source: https://leetcode.com/problems/count-ways-to-build-good-strings/
+    
+    CODE:                                                                    */
+
+class Solution {
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        vector<int> dp(high + 1);
+        dp[0] = 1;
+        int md = 1e9 + 7;
+        for (int i = min(zero, one); i <= high; i++) {
+            if(i>=zero){
+                dp[i] = (dp[i] + dp[i - zero]) % md;
+            }
+            if(i>=one){
+                dp[i] = (dp[i] + dp[i - one]) % md;
+            }
+        }
+        int sum = 0;
+        for (int i = low; i <= high; i++) {
+            sum = (sum + dp[i]) % md;
+        }
+        return sum;
+    }
+};
