905ec0b39eddf26e94fdc30c027fc3c3b9ecbec0
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-05-16 18:52:35 +0530

    Added Day16

---
+++ May_2023/Day16.cpp
@@ -0,0 +1,25 @@
+/*  Question: Swap Nodes in Pairs
+    Source: https://leetcode.com/problems/swap-nodes-in-pairs/
+    
+    CODE:                                                     */
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        } 
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* curr = dummy;
+        while (curr->next != nullptr && curr->next->next != nullptr) {
+            ListNode* first = curr->next;
+            ListNode* second = curr->next->next;
+            curr->next = second;
+            first->next = second->next;
+            second->next = first;
+            curr = curr->next->next;
+        }
+        return dummy->next;
+    }
+};
