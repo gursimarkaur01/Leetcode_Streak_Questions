f9747c3b25802eb60fd081bf935368c8848830a4
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-05-19 20:30:59 +0530

    Added Day19

---
+++ May_2023/Day19.cpp
@@ -0,0 +1,35 @@
+/*  Question: Is Graph Bipartite
+    Source: https://leetcode.com/problems/is-graph-bipartite/
+     
+    CODE:                                                     */
+
+class Solution {
+private :
+    bool bfs(int c,int s,vector<int>& color , vector<vector<int>>& graph){
+          queue<int> q;
+          q.push(s);
+          while(!q.empty()){
+              int node = q.front();
+              q.pop();
+              for(auto it:graph[node]){
+                  if(color[it] == -1){
+                      color[it] = !color[node];
+                      q.push(it);
+                  }
+                  else if(color[it] == color[node])
+                   return false;
+              }
+          }
+        return true;  
+    }
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+          int n = graph.size();
+          vector<int> color(n,-1);
+           for(int i=0;i<n;i++){
+               if(!bfs(0,i,color,graph))
+                return false;
+           }
+        return true;
+    }
+};
