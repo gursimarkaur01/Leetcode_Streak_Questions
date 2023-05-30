eeeebba0d7e06bfc4fa3ca4ccd2f296255e47c75
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-05-30 20:32:28 +0530

    Added Day30

---
+++ May_2023/Day30.cpp
@@ -0,0 +1,22 @@
+/*  Queston:  Design Hashset
+    Source: https://leetcode.com/problems/design-hashset/
+    
+    CODE:                                                     */
+
+class MyHashSet {
+private:
+    std::vector<bool> mp;
+public:
+    MyHashSet() {
+        mp.resize(1000001, false);
+    }
+    void add(int key) {
+        mp[key] = true;
+    }
+    void remove(int key) {
+        mp[key] = false;
+    }
+    bool contains(int key) {
+        return mp[key];
+    }
+};
