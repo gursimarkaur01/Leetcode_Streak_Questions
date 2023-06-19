71172ba4fdd1c403159c13df7e72c26f0e4f1a7c
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-06-19 23:33:14 +0530

    Added Day19

---
+++ June_2023/Day19.cpp
@@ -0,0 +1,17 @@
+/*  Question:  Find the Highest Altitude
+    Source:  https://leetcode.com/problems/find-the-highest-altitude/
+
+    CODE:                                                             */
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int maxAltitude = 0;
+        int currentAltitude = 0;
+        for (int i = 0; i < gain.size(); i++) {
+            currentAltitude += gain[i];
+            maxAltitude = max(maxAltitude, currentAltitude);
+        }
+        return maxAltitude;
+    }
+};
