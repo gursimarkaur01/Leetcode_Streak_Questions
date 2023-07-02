b899ca1c6cb46ee3ed3dac648a4849e2a8ec98d0
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-07-02 23:23:59 +0530

    Added Day2

---
+++ July.cpp/Day2.cpp
@@ -0,0 +1,31 @@
+/*  Question:  Maximum Number of Achievable Transfer Requests
+    Source:  https://leetcode.com/problems/maximum-number-of-achievable-transfer-requests/
+
+    CODE:                                                                                  */
+
+class Solution {
+public:
+    int ans = 0;
+    void helper(int start, vector<vector<int>>& requests, vector<int>& indegree, int n, int count) {
+        if (start == requests.size()) {
+            for (int i = 0; i < n; i++) {
+                if (indegree[i] != 0) {
+                    return;
+                }
+            }
+            ans = max(ans, count);
+            return;
+        }
+        indegree[requests[start][0]]--;
+        indegree[requests[start][1]]++;
+        helper(start + 1,requests, indegree, n, count + 1);
+        indegree[requests[start][0]]++;
+        indegree[requests[start][1]]--;
+        helper(start + 1,requests, indegree, n, count);
+    }    
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        vector<int> indegree(n, 0);
+        helper(0, requests, indegree, n, 0);
+        return ans;
+    }
+};
