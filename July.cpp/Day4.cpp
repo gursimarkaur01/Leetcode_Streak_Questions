e4389ca1df64a3396c019d94f3df2984f74d8df1
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-07-04 23:47:04 +0530

    Added Day4

---
+++ July.cpp/Day4.cpp
@@ -0,0 +1,20 @@
+/*  Question:  Single Number II
+    Source: https://leetcode.com/problems/single-number-ii/ 
+
+    CODE:                                                   */
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for(auto x: nums){
+            m[x]++;
+        }
+        for(auto x: m){
+            if(x.second == 1){
+                return x.first;
+            }
+        }
+        return -1;
+    }
+};
