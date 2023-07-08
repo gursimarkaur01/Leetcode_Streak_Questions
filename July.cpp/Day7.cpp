788d661714ced76f88b6a1a5215fb9e1a63ac398
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-07-08 21:59:04 +0530

    Update Day7.cpp

--- July.cpp/Day7.cpp
+++ July.cpp/Day7.cpp
@@ -1,4 +1,23 @@
 /*  Question:  
-    Source:  
+    Source:  https://leetcode.com/problems/maximize-the-confusion-of-an-exam/
 
     CODE:                                                                      */
+
+class Solution {
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        int maxFreq = 0;
+        int i = 0;
+        unordered_map<char, int> charCount;
+        for (int j = 0; j < answerKey.length(); j++) {
+            char currentChar = answerKey[j];
+            charCount[currentChar]++;
+            maxFreq = max(maxFreq, charCount[currentChar]);
+            if (j - i + 1 > maxFreq + k) {
+                charCount[answerKey[i]]--;
+                i++;
+            }
+        }
+        return answerKey.length() - i;
+    }
+};
