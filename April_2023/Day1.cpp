5569af29193df1d4b31d00b3cd5035827cc3c158
Gursimar Kaur Sodhi <115497263+gursimarkaur01@users.noreply.github.com>
2023-07-30 23:57:53 +0530

    Update Day1.cpp

--- April_2023/Day1.cpp
+++ April_2023/Day1.cpp
@@ -8,6 +8,7 @@ public:
     int search(vector<int>& nums, int target) {
         int beg = 0;
         int end = nums.size() - 1;
+        
         while (beg <= end) {
             int mid = beg + (end - beg) / 2;
             if (nums[mid] == target) {
